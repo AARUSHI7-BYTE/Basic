d379015c4f089d5336fdb98cbee776c6c80f62bf
aaru <aaru@123gmail.com>
2025-11-24 13:44:54 +0530

    merge

---
+++ merge.js
@@ -0,0 +1,25 @@
+arr1 = [1, 2, 3];
+arr2 = [4, 5];
+const merged=[...arr1,...arr2];
+console.log(merged);
+
+
+function sum(...nums) {
+  return nums.reduce((total, n) => total + n, 0);
+}
+console.log(sum(1, 2, 3));
+
+const user = {
+  name: "Alice",
+  age: 22,
+  address: {
+    city: "Bangalore",
+    pin: 560001
+  }
+};
+const{address:{city,pin}}=user;
+console.log(user.name);
+console.log(city);
+console.log(pin);
+
+
