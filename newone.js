dcc6a3a8ff731fe23e980e18090d2012c614a6e5
aaru <aaru@123gmail.com>
2025-11-25 14:35:54 +0530

    11,12,13,14

--- newone.js
+++ newone.js
@@ -53,3 +53,58 @@ console.log(sortedCategories);
 
 
 
+//14
+const employees =[
+
+{ name: "Alice", tasksCompleted: 8, rating: 4.7 },
+
+{ name: "Bob", tasksCompleted: 4, rating: 4.0 },
+
+{ name: "Charlie", tasksCompleted: 6, rating: 3.5 },
+
+{ name: "David", tasksCompleted: 10, rating: 4.9 },
+
+{ name: "Eve", tasksCompleted: 7, rating: 2.8 }
+
+];
+const performancePriority = {
+  "Excellent": 3,
+  "Good": 2,
+  "Needs Improvement": 1
+};
+
+const performance = employees
+.filter(employees=>employees.tasksCompleted>5)
+.map(employees=>{
+    if (employees.rating>=4.5){
+         level = "Excellent";
+    }
+    else if(3<employees.rating<4.5){
+        level = "Good";
+    }
+    else{
+        level= "Needs Improvement";
+    }
+      return {
+      name: employees.name,
+      performance: level
+    };
+}).sort((a,b)=> 
+    {return performancePriority[b.performance] - performancePriority[a.performance];
+    });
+
+console.log(performance);
+
+
+const hi =[
+
+{ name: "Alice", tasksCompleted: 8, rating: 4.7 },
+
+{ name: "Bob", tasksCompleted: 4, rating: 4.0 },
+
+{ name: "Charlie", tasksCompleted: 6, rating: 3.5 },
+
+{ name: "David", tasksCompleted: 10, rating: 4.9 },
+
+{ name: "Eve", tasksCompleted: 7, rating: 2.8 }
+];
