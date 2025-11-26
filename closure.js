6ae860c597d4e19b40b676a669b11e895ade32b4
aaru <aaru@123gmail.com>
2025-11-26 13:44:36 +0530

    21

--- closure.js
+++ closure.js
@@ -8,3 +8,55 @@ function changeAge(){
     }
 displayAge();
 changeAge();
+
+
+//20
+let count =0;
+function createCounter(){
+    function increment(){
+        count++;
+        console.log(count);
+    }
+    function decrement(){
+        count--;
+        console.log(count);
+    }
+    return {increment,decrement};
+}
+
+const counter = createCounter();
+counter.increment();
+counter.increment();
+counter.decrement();
+counter.decrement();
+
+function createBankAccount() {
+    let balance = 0; // private variable
+
+    function deposit(amount) {
+        balance += amount;
+        console.log(`Deposited: ${amount}`);
+    }
+
+    function withdraw(amount) {
+        if (amount > balance) {
+            console.log("Insufficient balance");
+        } else {
+            balance -= amount;
+            console.log(`Withdrawn: ${amount}`);
+        }
+    }
+
+    function getBalance() {
+        return balance;  // controlled access
+    }
+
+    return { deposit, withdraw, getBalance };
+}
+
+const account = createBankAccount();
+account.deposit(500);
+account.withdraw(200);
+account.withdraw(400);
+console.log(account.balance); 
+console.log(account.getBalance());
